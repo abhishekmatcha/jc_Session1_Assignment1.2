adec8a2e0dffb4263fdef86ce628a9a52cc962be
abhishekmatcha <seshaabhishek@gmail.com>
2017-01-30 00:19:49 +0530

    Add files via upload

---
+++ Prog.java
@@ -0,0 +1,12 @@
+class Prog
+{
+	public static void main(String args[])
+	{
+      int a=20,b=10; 
+      b= a-- - --a;
+      int c=a--;
+      int d=a>>2;
+      int e=a&b;
+      System.out.println("a="+a+"b="+b+"c="+c+"d="+d+"e="+e);
+	}
+}
